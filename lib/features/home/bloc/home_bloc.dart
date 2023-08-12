1768126df04ea00f84874a26f205940d6413494c
Fahim <ashfaq.itsd@goldenharvestbd.com>
2023-08-12 16:43:56 +0600

    bloc event

--- lib/features/home/bloc/home_bloc.dart
+++ lib/features/home/bloc/home_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
@@ -6,8 +8,27 @@ part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   HomeBloc() : super(HomeInitial()) {
-    on<HomeEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<HomeProductWishListButtonClikedEvent>(
+        homeProductWishListButtonClikedEvent);
+
+    on<HomeProductCartButtonClickedEvent>(homeProductCartButtonClickedEvent);
+
+    on<HomeWishListButtonNavigateEvent>(homeWishListButtonNavigateEvent);
+
+    on<HomeCartButtonNavigateEvent>(homeCartButtonNavigateEvent);
   }
+
+  FutureOr<void> homeProductWishListButtonClikedEvent(
+      HomeProductWishListButtonClikedEvent event, Emitter<HomeState> emit) {}
+
+  FutureOr<void> homeProductCartButtonClickedEvent(
+      HomeProductCartButtonClickedEvent event, Emitter<HomeState> emit) {}
+
+  FutureOr<void> homeWishListButtonNavigateEvent(
+      HomeWishListButtonNavigateEvent event, Emitter<HomeState> emit) {
+    print("wish list button navigate event");
+  }
+
+  FutureOr<void> homeCartButtonNavigateEvent(
+      HomeCartButtonNavigateEvent event, Emitter<HomeState> emit) {}
 }
